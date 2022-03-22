0c4f54407d889cbac4b7120c8aa727ad0ab2196d
Brandon Paul Davis <brandod8@gmail.com>
2022-03-21 22:53:07 -0500

    stable version 1.0

--- floats.s
+++ floats.s
@@ -16,48 +16,56 @@ ldr     x19, =levels                            //aquire pointer to string of ra
 ldr     d0, [x25, 0]                            //get the const pi value from x25 array store in scratch d0
 ldr     d1, [x25, 8]                            //get const value 2(double) from x25 array store in scratch d1
 fmul    d4, d0, d1                              //do some floating multiplication on stored pi value and 2 store in scratch d4
-ldr     d20, [x25, 32]                          //load d20 with double 0 value from const pool x25
-fmov    d28, d20                                //store this zero float value in d28 register for faster access later into the program
-fmov    d21, d20                                //set d21 to zero double value from d20
+ldr     x20, [x25, 32]                          //load d20 with double 0 value from const pool x25
+mov     x28, x20                                //store this zero float value in d28 register for faster access later into the program
+mov     x21, x20                                //set d21 to zero double value from d20
 ldr     x27, =LINES                             //save pointer to external LINES symbol
 ldr     x26, =COLS                              //save pointer to external COLS symbol
 ldr     w1, [x26]                               //load intger value from Cols pointer saved in x26
-ucvtf   d2, w1                                  //convert intger w1 to a float in d2
+SCVTF   d2, w1                                  //convert intger w1 to a float in d2
 fdiv    d22, d4, d2                             //floating point diviion between d4 and d2 stored in d22 which is backed up
-
+fmov    x22, d22
 
 top:
 bl      erase                                   //erase screen
+fmov    d20, x20
+fmov    d22, x22
 fadd    d20, d20, d22                           //float add d20 and d22 place into d20
+fmov    x20, d20
 mov     w23, wzr                                //zero w23 register
 
 sinner:
 ldr     w0, [x27]                               //load value of LINES into w0
-cmp     w0, w23                                 //compare if w23 is greater then or equal to w0 if so then branch to binner
+cmp     w23, w0                                 //compare if w23 is greater then or equal to w0 if so then branch to binner
 bge     bottom                                  //^   
-fmov    d21, d28                                //move zero double register d28 to double d21
+mov     x21, x28                                //move zero double register d28 to double d21
 mov     w24, wzr                                //zero register w24
 
 tinner:
 ldr     w0, [x26]                               //load COLS into w0
-cmp     w0, w24                                 //compare if w24 is greater then or equal to 20 if so then branch to binner
+cmp     w24, w0                                 //compare if w24 is greater then or equal to 20 if so then branch to binner
 bge     binner                                  //^
 
+fmov    d20, x20
+fmov    d21, x21
 fadd    d0, d20, d21
 bl      sin
 fmov    d3, #1.0
 fmov    d4, #2.0
 fmov    d5, #10.0
 fadd    d0, d0, d3
-fmul    d1, d4, d5
-fdiv    d0, d0, d1
-FCVTZU  x3, d0
+fdiv    d0, d0, d4
+fmul    d0, d0, d5
+FCVTZS  x3, d0
 
 mov     w0, w23                                 //move w23 to w0
 mov     w1, w24                                 //move w24 to w1 
-add     x2, x19, x3                             //add x3 to x19 to get the offset of char
+ldrb    w2, [x19, x3]
 bl      mvaddch                                 //run some ncurses function that adds a char
+fmov    d21, x21
+fmov    d22, x22
 fadd    d21, d21, d22                           //float addition d21 adds to d22 then stores in d21
+fmov    x21, d21
 add     w24, w24, 1                             //increment w24 by 1
 b       tinner
 
@@ -68,8 +76,8 @@ b       sinner
 bottom:
 ldr     x3, =stdscr                             //gets pointer to stdscr store int scratch x0     
 ldr     x0, [x3]           
-mov     x1, xzr                                 //zero x1 and x2
-mov     x2, xzr                                 //^
+mov     w1, wzr                                 //zero x1 and x2
+mov     w2, wzr                                 //^
 bl      box                                     //run ncurse box function
 bl      refresh                                 //refresh window
 b  top
